86c8272717267a997677ed502c03c29f7399b298
Dan Gunter <dkgunter@lbl.gov>
2015-08-03 18:16:58 -0700

    saving some work

--- lib/biokbase/data_api/display.py
+++ lib/biokbase/data_api/display.py
@@ -4,8 +4,13 @@ Objects for displaying the results in the IPython notebook.
 __author__ = 'Dan Gunter <dkgunter@lbl.gov>'
 __date__ = '8/1/15'
 
+from IPython.display import display
 import pandas as pd
-import qgrid
+try:
+    import qgrid
+except ImportError:
+    qgrid = None
+from jinja2 import Template
 
 _nbviewer = False
 def nbviewer_mode(value=None):
@@ -18,22 +23,30 @@ def nbviewer_mode(value=None):
         _nbviewer = bool(value)
     return _nbviewer
 
-class Contigs(pd.DataFrame):
+class Table(pd.DataFrame):
+    """Create a Table from the input data.
+    This is a thin wrapper around the Pandas DataFrame object.
+    """
+    def _ipython_display(self):
+        if qgrid:
+            return qgrid.show_grid(self, remote_js=nbviewer_mode())
+        else:
+            return display(self)
 
-    DEFAULT_SEP = '_'
-
-    def __init__(self, contigs, sep=DEFAULT_SEP):
+class Contigs(Table):
+    def __init__(self, contigs):
         """Create contigset from list of strings.
 
         Args:
           contigs: List of strings for contigs
-          sep: Separator between items packed into each string, e.g.
-                      'NODE_48_length_21448_cov_4.91263_ID_95'
         """
-        if not contigs:
-            pd.DataFrame.__init__(self, {})
-            return
+        Table.__init__(self, {'ids': contigs})
 
+    def __rb_parsing(self):
+        """Saved some parsing of what was found
+        in a Rhodobacter genome.
+            NODE_48_length_21448_cov_4.91263_ID_95
+        """
         c0 = contigs[0]
         colnames = c0.split(sep)[::2]
 
@@ -60,7 +73,25 @@ class Contigs(pd.DataFrame):
 
         # create DataFrame from the dict
         pd.DataFrame.__init__(self, contig_dict)
-        self._q = qgrid.show_grid(self, remote_js=nbviewer_mode())
 
-    def _ipython_display_(self):
-        return self._q._ipython_display_()
+class Classification(object):
+    __template = Template('''<h2>Classification</h2>
+    <ul>
+    {% for name in classification %}
+    <li style="margin-left: {{ loop.iter0 * 10 }}">
+    {{ name }}
+    </li>
+    {% endfor %}
+    </ul>
+    ''')
+    def __init__(self, taxon):
+        children = taxon.get_children() or []
+        tx, parents = taxon, []
+        while tx:
+            tx = tx.get_parent()
+            if tx:
+                parents.insert(tx.get_scientific_name(), 0)
+        self.classf = parents + [taxon.get_scientific_name()] + children
+
+    def _repr_html_(self):
+        return self.__template.render(classification=self.classf)
