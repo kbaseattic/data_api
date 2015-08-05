97e549def2c31f0b58addee823efd3ffaee5c8e6
Matt Henderson <mhenderson@lbl.gov>
2015-08-05 10:58:34 -0700

    attempting to merge requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,20 @@ config = {
     "version": "0.1",
     "setup_requires": ["six"],
     "tests_require": ["nose"],
-    "install_requires": ["falcon", "requests", "six"],
+    "install_requires": [
+        "Jinja2==2.7.3",
+        "Sphinx==1.3.1",
+        "guzzle-sphinx-theme==0.7.2",
+        "nose",
+        "python-coveralls",
+        "six==1.9.0",
+        "enum34",
+        "pandas==0.16.2",
+        "qgrid==0.1.1",
+        "matplotlib==1.4.3",
+        "requests==2.7.0",
+        "notebook==4.0.1"
+    ],
     "packages": ["biokbase","biokbase.data_api","biokbase.data_api.tests","biokbase.workspace"],
     "scripts": ["bin/data_api_test_basic.py", "bin/data_api_demo.py"],
     "name": "genome_api"
