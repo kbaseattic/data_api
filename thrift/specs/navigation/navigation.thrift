840419da5d0e14e3ddc3ba189560122cc8af464d
Dan Gunter <dkgunter@lbl.gov>
2015-09-14 16:55:25 -0700

    save work

--- thrift/specs/navigation/navigation.thrift
+++ thrift/specs/navigation/navigation.thrift
@@ -24,7 +24,27 @@ exception AuthException {
  * ---------------
  */
 
-typedef string ObjectReference
+/**
+ * Version of an object.
+ * May either be a number, as in Subversion,
+ * or a string which would accommodate a hash, as in Git.
+ *
+ * @param seqno Numeric version
+ * @param hash String version
+ */
+union ObjectVersion {
+   1: i32 seqno,
+   2: string hash
+}
+
+/** Object reference.
+ * @param id Unique object identifier
+ * @param version Object version
+ */
+struct ObjectReference {
+    1: required string id,
+    2: optional ObjectVersion version
+}
 
 /** Globally unique identifier. */
 typedef string ContainerID
@@ -61,23 +81,27 @@ struct Context {
  * A project container.
  *
  * @param id Unique identifier for the container
+ * @param name Human-readable name for container (may not be unique)
  * @param created When the project was created
  * @param modified When the project was modified
  */
 struct Project {
    1: required ContainerID id,
-   2: optional ContainerName name
+   2: optional ContainerName name,
+   10: optional TimestampMs created,
+   11: optional TimestampMs modified
 }
 
 /**
  * A workspace container.
  *
  * @param id Unique identifier for the container
+ * @param name Human-readable name for container (may not be unique)
  * @param created When the workspace was created
  * @param modified When the workspace was modified
  */
  struct Workspace {
-    1: requred ContainerID id,
+    1: required ContainerID id,
     2: optional ContainerName name,
    10: optional TimestampMs created,
    11: optional TimestampMs modified
@@ -88,7 +112,7 @@ struct Project {
  *
  * @param ref Unique object reference
  * @param type Name of object type
- * @param name Name of the object itself
+ * @param name Human-readable name of the object (may not be unique)
  * @param created When the object was created
  * @param modified When the object was modified
  * @param size Size of serialized uncompressed object, in bytes
@@ -108,12 +132,24 @@ struct Object {
 struct Filter {
     1: optional SearchExpr name,
     2: optional SearchExpr type,
-    3: optional i64 size_min,
-    4: optional i64 size_max,
-    5: optional TimestampMs created_min,
-    6: optional TimestampMs created_max,
-    5: optional TimestampMs modified_min,
-    6: optional TimestampMs modified_max
+    3: optional VersionExpr version,
+    10: optional TimestampMs created_min,
+    11: optional TimestampMs created_max,
+    12: optional TimestampMs modified_min,
+    13: optional TimestampMs modified_max,
+    14: optional i64 size_min,
+    15: optional i64 size_max,
+}
+
+/**
+ * Expression to find version(s) of an object,
+ * based either on a numeric sequence or
+ * on match with a string ("hash").
+ */
+struct VersionExpr {
+  1: optional ObjectVersion min_seqno,
+  2: optional ObjectVersion max_seqno,
+  3: string hash
 }
 
 /*
@@ -132,44 +168,44 @@ service thrift_service {
      * @throws ServiceException for permissions errors, etc.
      */
      Context
-     create_context(1: optional ContainerName project = NONAME,
-                    2: optional ContainerName workspace = NONAME)
-     throws (ServiceException, AuthException)
+     create_context(1: ContainerName project = NONAME,
+                    2: ContainerName workspace = NONAME)
+      throws (1: ServiceException serr, 2: AuthException aerr)
 
     /**
      * Set current project within given Context.
      */
     void
-    set_project(1: required Context context,
-                     2: required ContainerName project)
-             throws (ServiceException, AuthException)
+    set_project(1: Context context,
+                2: ContainerName project)
+             throws (1: ServiceException serr, 2:AuthException aerr)
     /**
      * Set current workspace within given Context.
      */
     void
-    set_workspace(1: required Context context,
-                  2: required ContainerName workspace)
-    throws (ServiceException, AuthException)
+    set_workspace(1: Context context,
+                  2: ContainerName workspace)
+    throws (1: ServiceException serr, 2: AuthException aerr)
 
     /**
      * List selected objects within current context.
      * With no parameters, this lists all objects.
      */
     list<Object>
-    list_objects(1: required Context context,
-                 2: optional Filter filter)
-    throws (ServiceException)
+    list_objects(1: Context context,
+                 2: Filter filter)
+    throws (1: ServiceException serr)
 
     /**
      * Find objects, optionally within a given Context.
      * With no parameters, this finds all objects in all contexts.
      */
     map<Context,list<Object>>
-    find_objects(1: optional Context context,
-                 2: optional Filter project_filter,
-                 3: optional Filter workspace_filter,
-                 4: optional Filter object_filter)
-    throws (ServiceException)
+    find_objects(1: Context context,
+                 2: Filter project_filter,
+                 3: Filter workspace_filter,
+                 4: Filter object_filter)
+    throws (1: ServiceException serr)
 
     /**
      * Copy object from its current context to another.
@@ -177,7 +213,7 @@ service thrift_service {
      void
      copy_object(1: required ObjectReference ref,
                  2: required Context destination)
-     throws (ServiceException, AuthException)
+     throws (1: ServiceException serr, 2: AuthException aerr)
 
     /**
      * Move object from its current context to another.
@@ -186,13 +222,13 @@ service thrift_service {
      void
      move_object(1: required ObjectReference ref,
                  2: required Context destination)
-     throws (ServiceException, AuthException)
+     throws (1: ServiceException serr, 2: AuthException aerr)
 
     /**
      * Delete object.
      */
      void
      delete_object(1: required ObjectReference ref)
-     throws (ServiceException, AuthException)
+     throws (1: ServiceException serr, 2: AuthException aerr)
 
 }
