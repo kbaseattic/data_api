6901c65ca005c49cc2abb922ca30f7761230dee8
Dan Gunter <dkgunter@lbl.gov>
2015-09-21 09:05:15 -0700

    make sure test works with local file data

--- lib/doekbase/data_api/tests/examples/taxon_api.py
+++ lib/doekbase/data_api/tests/examples/taxon_api.py
@@ -11,12 +11,19 @@ import os
 # Local
 from doekbase.data_api.taxonomy.taxon.api import TaxonAPI
 
-def fetch_and_print_taxon():
+def fetch_and_print_taxon(ws_url='https://ci.kbase.us/services/ws/'):
+    """Fetch Taxon from Workspace Service.
+
+    Args:
+      ws_url (str): a service address or a local directory
+         containing a downloaded version of the data created with the
+         ``dump_wsfile`` utility.
+    """
     # Initialize API with target object
     taxon_ref = 'ReferenceTaxons/242159_taxon'
+    # Try to connect to remote service
     taxon = TaxonAPI(token=os.environ.get('KB_AUTH_TOKEN'), ref=taxon_ref,
-                     services={'workspace_service_url':
-                                   'https://ci.kbase.us/services/ws/'})
+                     services={'workspace_service_url': ws_url})
     # Now the methods of the instance can be used to fetch and show
     # taxonomic information
     print('Got taxon "{} ({})"'.format(taxon.get_scientific_name(),
--- lib/doekbase/data_api/tests/examples/test_taxon_api.py
+++ lib/doekbase/data_api/tests/examples/test_taxon_api.py
@@ -6,7 +6,12 @@ When you add a new example, add a call to it, here.
 __author__ = 'Dan Gunter <dkgunter@lbl.gov>'
 __date__ = '9/19/15'
 
-from .taxon_api import *
+from doekbase.data_api.tests.examples.taxon_api import *
+from doekbase.data_api.tests import shared
+
+def setup():
+    shared.setup()
 
 def test_examples():
-    fetch_and_print_taxon()
+    url = shared.services['workspace_service_url']
+    fetch_and_print_taxon(url)
