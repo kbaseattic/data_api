c42d25857064f1f22de1508547490cbea8065d17
Matt Henderson <mhenderson@lbl.gov>
2015-09-24 22:39:17 -0700

    revamped install, dropped requests version to 2.3.0 for KBase compatibility, split ipython/jupyter and rendering modules to a separate requirements file, added --jupyter option, using find_packages to build the package list, updated travis to reflect new requirements files and cleaned out unused commented lines

--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 import setuptools
 import os
+import sys
 
-def parse_requirements():
+def parse_requirements(filename):
     packages = list()
     
-    with open("requirements.txt", 'r') as req_file:
+    with open(filename, 'r') as req_file:
         req_lines = req_file.read().splitlines()
         
         for line in req_lines:
-            if line.strip() == "" or line.startswith("-"):
+            if line.strip() == "":
                 pass
+            elif line.startswith("-r"):
+                packages.extend(parse_requirements(line.split(" ")[-1]))
             else:
                 packages.append(line)
     return packages
 
 version = open('VERSION').read().strip()
+packages = setuptools.find_packages("lib")
+setup_args = sys.argv[1:]
+
+if "--jupyter" in setup_args:
+    setup_args.remove("--jupyter")
+    install_requires = parse_requirements(
+        os.path.join(os.path.dirname(__file__),"requirements-jupyter.txt"))
+else:
+    install_requires = parse_requirements(
+        os.path.join(os.path.dirname(__file__),"requirements.txt"))
 
 config = {
     "description": "KBase Data API",
@@ -24,20 +37,9 @@ config = {
     "author_email": "mhenderson@lbl.gov",
     "version": version,
     "setup_requires": ["six"],
+    "install_requires": install_requires,
     "tests_require": ["nose", "nose-timer", "codecov"],
-    "install_requires": parse_requirements(),
-    "packages": ["doekbase",
-                 "doekbase.data_api",
-                 "doekbase.data_api.annotation",
-                 "doekbase.data_api.sequence",
-                 "doekbase.data_api.taxonomy",
-                 "doekbase.data_api.taxonomy.taxon",
-                 "doekbase.data_api.taxonomy.taxon.service",
-                 "doekbase.data_api.baseobj",
-                 "doekbase.data_api.tests",
-                 "doekbase.data_api.tests.performance",
-                 "doekbase.data_api.tests.examples",
-                 "doekbase.workspace"],
+    "packages": packages,
     "scripts": ["bin/data_api_demo.py",
                 "bin/data_api_benchmark.py",
                 "bin/dump_wsfile"],
@@ -51,4 +53,5 @@ config = {
 }
 
 setuptools.setup(package_dir = {'': 'lib'},
+                 script_args=setup_args,
                  **config)
