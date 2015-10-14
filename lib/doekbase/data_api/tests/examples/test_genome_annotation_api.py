0bb5a86b3019e93c38a0f02373028e3c03b979d1
Dan Gunter <dkgunter@lbl.gov>
2015-10-14 10:37:51 -0700

    added first cut at annotation api docs page

--- lib/doekbase/data_api/tests/examples/test_genome_annotation_api.py
+++ lib/doekbase/data_api/tests/examples/test_genome_annotation_api.py
@@ -1,19 +1,21 @@
-
-    """
+"""
 Verify that all examples in ``taxon_api`` are runnable.
 
 When you add a new example, add a call to it, here.
 """
-
 __author__ = 'Dan Gunter <dkgunter@lbl.gov>'
-__date__ = '9/19/15'
+__date__ = '10/14/15'
 
-from doekbase.data_api.tests.examples.taxon_api import *
+from doekbase.data_api.tests.examples.genome_annotation_api import *
 from doekbase.data_api.tests import shared
+
+# Some known reference genomes
+genome_new = "PrototypeReferenceGenomes/kb|g.166819"
+genome_old = "OriginalReferenceGenomes/kb|g.166819"
 
 def setup():
     shared.setup()
 
 def test_examples():
     url = shared.services['workspace_service_url']
-    fetch_and_print_taxon(url)
+    get_proteins_for_gene(genome_new, 'some gene', ws_url=url)
