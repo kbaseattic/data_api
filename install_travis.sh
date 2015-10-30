ba0a4c7f51652c726b02ab6971e24b18461d8690
Matt Henderson <mhenderson@lbl.gov>
2015-10-29 23:14:18 -0700

    running travis install script

--- install_travis.sh
+++ install_travis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ THRIFT_CACHED -eq 0 ]] ; then
-    apt-get update -qq
-    apt-get install -qq libboost-dev libboost-test-dev \
+    sudo apt-get update -qq
+    sudo apt-get install -qq libboost-dev libboost-test-dev \
 libboost-program-options-dev libevent-dev automake libtool \
 flex bison pkg-config g++ libssl-dev \
 libbit-vector-perl libclass-accessor-class-perl
@@ -11,11 +11,11 @@ libbit-vector-perl libclass-accessor-class-perl
     tar xfz thrift-0.9.2.tar.gz
     cd thrift-0.9.2
     ./configure --without-ruby
-    make install
+    sudo make install
     cd -
 elif [[ THRIFT_CACHED -eq 1 ]] ; then
     cd $HOME/thrift/thrift-0.9.2/
     ./configure --without-ruby
-    make install
+    sudo make install
     cd -
 fi
