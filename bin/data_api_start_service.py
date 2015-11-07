5513ba4b43e7f0ff71a873a4010f6c825d365e45
Keith Keller <kkeller@lbl.gov>
2015-11-06 16:23:07 -0800

    Improved support for arbirtary service deployments

--- bin/data_api_start_service.py
+++ bin/data_api_start_service.py
@@ -36,9 +36,9 @@ def main():
     parser.add_argument("--port", help="port to listen on", type=int)
     parser.add_argument("--kbase_url", help="prod, next, ci, localhost, dir", default="dir")
     parser.add_argument("--pidfile", help="path to pidfile to use")
-    parser.add_argument('--verbose', '-v', dest='vb', action="count", default=0,
+    parser.add_argument('--verbose', '-v', dest='vb', action="count", default=1,
                         help="Print more verbose messages to standard error. "
-                             "Repeatable. (default=ERROR)")
+                             "Repeatable. (default=INFO)")
 
     args = parser.parse_args()
 
@@ -46,18 +46,17 @@ def main():
     verbosity = (logging.ERROR, logging.INFO, logging.DEBUG)[min(args.vb, 2)]
     g_log.setLevel(verbosity)
 
-    pidfilename = "TaxonAPI.pid"
-
     # Parse configuration
     config = ConfigParser.ConfigParser()
     if args.config:
         config.read(args.config)
 
     if args.service not in SERVICE_NAMES:
-        print "Unrecognized service name {}".format(args.service)
+        g_log.info("Unrecognized service name {}".format(args.service))
         sys.exit(1)
 
     global_stanza_name = 'data_api' + ".kbase." + args.kbase_url
+    
     service_stanza_name = args.service + '_api'
     # set defaults if nothing is set anywhere
     services = None
@@ -67,8 +66,9 @@ def main():
     redis_port = None
 
     # get config
+    g_log.info("Attempting to read config from " + args.config)
     if config.has_section(global_stanza_name):
-        print "Reading global config from file " + args.config + " and stanza " + global_stanza_name
+        g_log.info("Reading global config from stanza " + global_stanza_name)
         ws = config.get(global_stanza_name,'workspace_service_url')
         shock = config.get(global_stanza_name,'shock_service_url')
         services = {'workspace_service_url': ws,
@@ -81,7 +81,7 @@ def main():
             redis_host = None
             redis_port = None
     if config.has_section(service_stanza_name):
-        print "Reading " + service_name + " config from file " + args.config + " and stanza " + service_stanza_name
+        g_log.info("Reading " + args.service + " config from stanza " + service_stanza_name)
         if config.has_option(service_stanza_name,'service-port'):
             service_port = config.getint(service_stanza_name,'service-port')
         if config.has_option(service_stanza_name,'pidfile'):
@@ -93,11 +93,14 @@ def main():
     if args.port:
         service_port=args.port
 
+    if service_port is None:
+        g_log.error("Service port not defined!")
+        return 1
+
     if redis_host is not None and redis_port is not None:
-        print "Activating REDIS at host:{} port:{}".format(redis_host,redis_port)
+        g_log.info("Activating REDIS at host:{} port:{}".format(redis_host,redis_port))
         cache.ObjectCache.cache_class = cache.RedisCache
         cache.ObjectCache.cache_params = {'redis_host': redis_host, 'redis_port': redis_port}
-
 
     # test that the pidfile is not already locked with a running process
     pidfile = lockfile.pidlockfile.PIDLockFile(pidfilename, timeout=-1)
@@ -127,11 +130,8 @@ def main():
         #TODO set up signal handling for HUP, KILL
         #signal.signal(signal.SIGHUP, driver.reload_config())
 
-        g_log.info("Starting service: port={:d}, PID={:d}".format(args.port,
-                                                                  pid))
+        g_log.info("Starting service {}: port={:d}, PID={:d}".format(args.service,int(service_port), int(pid)))
         try:
-            print "starting service on port " + str(service_port)
-
             if args.service == "taxon":
                 from doekbase.data_api.taxonomy.taxon.service import driver
             elif args.service == "assembly":
