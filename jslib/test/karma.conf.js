df55545b19856eac55c8fbfdf6f0e55056dc11d5
Dan Gunter <dkgunter@lbl.gov>
2015-11-18 17:10:16 -0800

    tweaks

--- jslib/test/karma.conf.js
+++ jslib/test/karma.conf.js
@@ -71,9 +71,13 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-//    reporters: ['progress', 'coverage'],
+    //reporters: ['progress', 'coverage'],
     reporters: ['progress'],
 
+//    coverageReporter: {
+//      type : 'html',
+//      dir : 'coverage/'
+//    },
 
     // web server port
     port: 9876,
