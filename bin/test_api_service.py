3280d51bbb174a714277ff44f9fa49310a793daf
Keith Keller <kkeller@lbl.gov>
2015-11-20 14:21:23 -0800

    Moved to use API client library instead of Thrif libs.

--- bin/test_api_service.py
+++ bin/test_api_service.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python
 
-from thrift import Thrift
-from thrift.transport import TSocket
-from thrift.transport import TTransport
-from thrift.protocol import TBinaryProtocol
-from thrift.protocol import TJSONProtocol
-from thrift.transport import THttpClient
-import os
-import sys
-
-
 def test_api_service(service_name='assembly',apiurl='http://localhost',token=None):
     try:
         if service_name == "taxon":
-            from doekbase.data_api.taxonomy.taxon.service import thrift_client
+            #from doekbase.data_api.taxonomy.taxon.service import thrift_client
+            from doekbase.data_api.taxonomy.taxon.api import TaxonClientAPI
+            api = TaxonClientAPI(apiurl, token, None)
         elif service_name == "assembly":
-            from doekbase.data_api.sequence.assembly.service import thrift_client
+            #from doekbase.data_api.sequence.assembly.service import thrift_client
+            from doekbase.data_api.sequence.assembly.api import AssemblyClientAPI
+            api = AssemblyClientAPI(apiurl, token, 'PrototypeReferenceGenomes/kb|g.166819_assembly')
         else:
             raise Exception("Service not activated: {}".format(service_name))
     except:
         print '3 ' + service_name + '_api - UNKNOWN - could not load client library'
-
-    transport=THttpClient.THttpClient(apiurl)
-    protocol = TBinaryProtocol.TBinaryProtocol(transport)
-    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
-    client = thrift_client.Client(protocol)
-
-    transport.open()
 
     if service_name == 'taxon':
         try:
@@ -36,11 +23,20 @@ def test_api_service(service_name='assembly',apiurl='http://localhost',token=Non
             print '2 ' + service_name + '_api - CRITICAL'
 
     if service_name == 'assembly':
-        stats = client.get_stats(os.environ['KB_AUTH_TOKEN'],'PrototypeReferenceGenomes/kb|g.166819_assembly')
-        if stats.num_contigs==21:
-            print '0 ' + service_name + '_api - OK - 21 contigs found'
+        stats = api.get_stats()
+        ref_num_contigs=21
+        # would be better to be in a try block
+        if stats['num_contigs']==ref_num_contigs:
+            print '0 ' + service_name + '_api - OK - ' + str(ref_num_contigs) + ' contigs found'
+        else:
+            print '2 ' + service_name + '_api - CRITICAL - ' + str(stats['num_contigs']) + ' contigs found, not ' + str(ref_num_contigs)
+
 
 if __name__ == "__main__":
+    import os
+    import sys
+    import argparse
+
     # should argparse these
     service_name=sys.argv[1]
     apiurl=sys.argv[2]
