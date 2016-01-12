2086c2256fda1d31eee56346c4da9a6246afa2e1
realmarcin <MJoachimiak@lbl.gov>
2016-01-11 21:26:10 -0800

    added comments

--- lib/doekbase/data_api/tests/examples/proteins_to_fasta.py
+++ lib/doekbase/data_api/tests/examples/proteins_to_fasta.py
@@ -10,11 +10,15 @@ import os
 def get_protein_fasta(data):
     i=0
     string =""
+    #for each protein in the annotation
     for keys in data:
         i=i+1
+        #limit it to 3 proteins for testing
         if (i >3):
             break
         geneinfo=data[keys]
+
+        #retrieve gene id alias, function, and peptide swquence data
         if(geneinfo["protein_aliases"] != None):
             aliases="".join(geneinfo["protein_aliases"])
             if(geneinfo["protein_function"] != None):
@@ -25,10 +29,15 @@ def get_protein_fasta(data):
 
 def run(ws_url='https://ci.kbase.us/services/ws/'):
 
+    #an example KBase reference genome
     genomeref = "ReferenceGenomeAnnotations/kb|g.166819"
+
+    #creating a new GenomeAnnotation object
     genome_annotation = GenomeAnnotationAPI(services = {"workspace_service_url": ws_url}, token=os.environ.get('KB_AUTH_TOKEN'), ref=genomeref)
         
+    #return all protein objects from this genome
     proteins= genome_annotation.get_proteins()
+    #format the proteins in FASTA format
     fasta = get_protein_fasta(proteins)
 
     outfile = '166819_prot.fasta'
