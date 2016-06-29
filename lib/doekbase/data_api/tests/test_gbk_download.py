dc455824f0f62589e103a1abe50d5c7ff0ade79b
realmarcin <MJoachimiak@lbl.gov>
2016-06-28 21:08:31 -0700

    using refactored test method

--- lib/doekbase/data_api/tests/test_gbk_download.py
+++ lib/doekbase/data_api/tests/test_gbk_download.py
@@ -13,28 +13,30 @@ import subprocess
 # KBase imports
 from doekbase.data_api.downloaders import GenomeAnnotation
 
+if __name__ == '__main__':
+    # logger = script_utils.stderrlogger(__file__, level=logging.DEBUG)
 
-if __name__ == '__main__':	
-	#logger = script_utils.stderrlogger(__file__, level=logging.DEBUG)
-	    
-	token = os.environ.get("KB_AUTH_TOKEN")
+    token = os.environ.get("KB_AUTH_TOKEN")
 
-	services = {"workspace_service_url": "https://ci.kbase.us/services/ws/",
-            "shock_service_url": "https://ci.kbase.us/services/shock-api/",
-            "handle_service_url": "https://ci.kbase.us/services/handle_service/"}
+    services = {"workspace_service_url": "https://ci.kbase.us/services/ws/",
+                "shock_service_url": "https://ci.kbase.us/services/shock-api/",
+                "handle_service_url": "https://ci.kbase.us/services/handle_service/"}
 
-	try:
-		print "starting"
-		GenomeAnnotation.downloadAsGBK("7824/GCF_000001735.3_TAIR10_genomic_GA", services, token, "test.gbk", "./")
-		print "done"
+    try:
 
-		GenomeAnnotation.testGBKDownload_vs_API(services, token, genome_ref, output_file_name)
-		print "done testing"
-		
-	except Exception, e:
-	    #ogger.exception(e)
-	    print "Exception"
-	    raise
-	    sys.exit(1)
+        genome_ref = "7824/GCF_000005845.2_ASM584v2_genomic_GA"
+        output_file_name = "test.gbk"
+        
+        print "starting"
+        GenomeAnnotation.downloadAsGBK("7824/GCF_000001735.3_TAIR10_genomic_GA", services, token, "test.gbk", "./")
+        print "done"
 
-	sys.exit(0)
+        GenomeAnnotation.testGBKDownload_vs_API(services, token, genome_ref, output_file_name)
+        print "done testing"
+
+    except Exception, e:
+        # ogger.exception(e)
+        print "Exception"
+        raise
+
+    sys.exit(0)
--- lib/doekbase/data_api/tests/test_gbk_download_ecoli.py
+++ lib/doekbase/data_api/tests/test_gbk_download_ecoli.py
@@ -14,33 +14,35 @@ import pkgutil
 # KBase imports
 from doekbase.data_api.downloaders import GenomeAnnotation
 
+if __name__ == '__main__':
+    # logger = script_utils.stderrlogger(__file__, level=logging.DEBUG)
 
-if __name__ == '__main__':	
-	#logger = script_utils.stderrlogger(__file__, level=logging.DEBUG)
-	    
-	token = os.environ.get("KB_AUTH_TOKEN")
+    token = os.environ.get("KB_AUTH_TOKEN")
 
-	services = {"workspace_service_url": "https://ci.kbase.us/services/ws/",
-            "shock_service_url": "https://ci.kbase.us/services/shock-api/",
-            "handle_service_url": "https://ci.kbase.us/services/handle_service/"}
+    services = {"workspace_service_url": "https://ci.kbase.us/services/ws/",
+                "shock_service_url": "https://ci.kbase.us/services/shock-api/",
+                "handle_service_url": "https://ci.kbase.us/services/handle_service/"}
 
-	for importer, modname, ispkg in pkgutil.walk_packages(path=None, onerror=lambda x: None):
-		print(modname)
+    # for importer, modname, ispkg in pkgutil.walk_packages(path=None, onerror=lambda x: None):
+    #		print(modname)
 
-	try:
-		print "starting"
-		genome_ref = "7824/GCF_000005845.2_ASM584v2_genomic_GA"
-		output_file_name = "test.gbk"
-		GenomeAnnotation.downloadAsGBK(genome_ref, services, token, output_file_name, "./")
-		print "done w upload"
+    try:
+        print "starting"
+        genome_ref = "7824/GCF_000005845.2_ASM584v2_genomic_GA"
+        output_file_name = "test.gbk"
 
-		GenomeAnnotation.testGBKDownload_vs_API(services, token, genome_ref, output_file_name)
-		print "done testing"
+        print dir(GenomeAnnotation)
 
-	except Exception, e:
-	    #ogger.exception(e)
-	    print "Exception"
-	    raise
-	    sys.exit(1)
+        GenomeAnnotation.downloadAsGBK(genome_ref, services, token, output_file_name, "./")
+        print "done w upload"
 
-	sys.exit(0)
+        print sys.path
+        GenomeAnnotation.testGBKDownload_vs_API(services, token, genome_ref, output_file_name)
+        print "done testing"
+
+    except Exception, e:
+        # logger.exception(e)
+        print "Exception"
+        raise
+
+    sys.exit(0)
